d64af6a08cea59964efa56ee0414eea58fa961f7
Cedric Anthony <dev+git@anthodev.net>
2020-06-18 08:19:35 +0200

    Added button to switch visibility of read news

--- assets/components/feed/FeedBar.vue
+++ assets/components/feed/FeedBar.vue
@@ -1,43 +1,53 @@
 <template>
-    <v-row class="mt-7 mb-2 ml-1">
-        <v-btn @click="markRead" :loading="loading" :disabled="disabled" class="mr-2" color="warning" normal>Mark everything as read</v-btn>
-        <v-select class="pa-0 col-3" :items="selectFilters" label="Show news since..." disabled solo dense hide-details></v-select>
-    </v-row>
+  <v-row class="mt-7 mb-2 ml-1">
+    <v-btn @click="markRead" :loading="loading" :disabled="disabled" class="mr-2" color="warning" normal>Mark everything as read</v-btn>
+    <v-btn @click="switchHideStatus()" v-if="user.settings.display_unread" class="mr-2" color="primary" normal>Hide read <v-icon right>mdi-eye-off</v-icon></v-btn>
+    <v-btn @click="switchHideStatus()" v-else class="mr-2" color="primary" normal>Show read <v-icon right>mdi-eye</v-icon></v-btn>
+    <v-select class="pa-0 col-3" :items="selectFilters" label="Show news since..." disabled solo dense hide-details></v-select>
+  </v-row>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            selectFilters: ['all', 'day', 'week', 'month'],
-            loading: false,
-            disabled: false
-        }
-    },
-
-    computed: {
-        id() {
-            let id = this.$route.params.id
-
-            if (id == undefined) id = 0
-
-            return id
-        }
-    },
-
-    methods: {
-        markRead() {
-            this.loading = true
-            this.disabled = true
-
-            this.$store.dispatch('SET_MARK_FEED_AS_READ', this.id).then(() => {
-                this.loading = false
-                this.disabled = false
-
-                this.$store.dispatch('SET_REFRESH_STATUS', true)
-            })
-        }
+  data() {
+    return {
+      selectFilters: ['all', 'day', 'week', 'month'],
+      loading: false,
+      disabled: false
     }
+  },
+
+  computed: {
+    id() {
+      let id = this.$route.params.id
+
+      if (id == undefined) id = 0
+
+      return id
+    },
+
+    user() {
+      return this.$store.getters.user
+    }
+  },
+
+  methods: {
+    markRead() {
+      this.loading = true
+      this.disabled = true
+
+      this.$store.dispatch('SET_MARK_FEED_AS_READ', this.id).then(() => {
+        this.loading = false
+        this.disabled = false
+
+        this.$store.dispatch('SET_REFRESH_STATUS', true)
+      })
+    },
+
+    switchHideStatus() {
+      console.log('switch')
+    }
+  }
 }
 </script>
 
